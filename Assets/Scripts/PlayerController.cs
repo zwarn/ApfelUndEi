cf7e635b11d12ff2762a698b734e115836666269
zwarn <Michael.Budahn@gmail.com>
2018-09-22 15:08:45 +0200

    Add Movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
-	public int _playerNumber;
-	public Color _playerColor;
+	public int PlayerNumber;
+	public Color PlayerColor;
+
+	private float _movementSpeed = 0.1f;
+	private Vector3 _lookingDirection = new Vector3();
 	
 	// Use this for initialization
 	void Start () {
@@ -16,5 +19,9 @@ public class PlayerController : MonoBehaviour
 	// Update is called once per frame
 	void Update () {
 		
+		// Movement
+		Vector3 deltaMovement = new Vector3(Input.GetAxis("Horizontal " + PlayerNumber), Input.GetAxis("Vertical " + PlayerNumber),0);
+		_lookingDirection = deltaMovement.normalized;
+		transform.Translate(deltaMovement * _movementSpeed);
 	}
 }
